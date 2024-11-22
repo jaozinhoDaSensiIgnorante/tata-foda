23123b3b0884298996bc55257f0abf4eb51cfac4
jaozinhoDaSensiIgnorante <0000107729007xsp@al.educacao.sp.gov.br>
2024-11-22 20:38:19 -0300

    Add files via upload
    aula da paulinha o loca

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual foi o intuito da guerra fria?',
+    'disputa pela hegemonia mundial entre Estados Unidos e União Soviética'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é a diferença entre um método e uma classe?',
+    'um conjunto de instruções que podem ser chamadas para serem executadas nos dados. '
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
